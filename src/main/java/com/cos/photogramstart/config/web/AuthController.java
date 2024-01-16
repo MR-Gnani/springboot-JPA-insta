2fffa219b68f2883db0a513a85c07748d876b97c
MR NANI <nani@MRui-MacBookAir.local>
2024-01-16 12:42:39 +0900

    2-5 유효성 검사 예외처리

--- src/main/java/com/cos/photogramstart/config/web/AuthController.java
+++ src/main/java/com/cos/photogramstart/config/web/AuthController.java
@@ -1,15 +1,22 @@
 package com.cos.photogramstart.config.web;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.cos.photogramstart.Service.AuthService;
 import com.cos.photogramstart.config.web.dto.auth.SignupDto;
 import com.cos.photogramstart.domain.user.User;
+import com.cos.photogramstart.handler.ex.CustomValidationException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,7 +25,7 @@ import lombok.RequiredArgsConstructor;
 public class AuthController {
 	
 	
-	private static final Logger log = LoggerFactory.getLogger(AuthController.class);
+	// private static final Logger log = LoggerFactory.getLogger(AuthController.class);
 	
 	@Autowired
 	private final AuthService authService;
@@ -41,15 +48,24 @@ public class AuthController {
 	
 	// 3. 회원가입 진행
 	@PostMapping("/auth/signup")
-	public String signup(SignupDto signupDto) { // key=value(x-www-form-urlencoded)
-		log.info(signupDto.toString());
+	// 오류가 발생하면 "bindingResult"의 "getFieldErrors" 컬렉션에 다 모아준다. 
+	// Controller에서 ResponseBody 어노테이션을 사용하면 데이터 리턴이 가능하다. 
+	public String signup(@Valid SignupDto signupDto, BindingResult bindingResult) { // key=value(x-www-form-urlencoded)
 		
-		// User <-- SignupDto
-		User user = signupDto.toEntity();
-		log.info(user.toString());
-		User userEntity = authService.회원가입(user);
-		System.out.println(userEntity);
-		return "auth/signin"; // 회원가입이 진행되면 로그인창으로 이동
+		if(bindingResult.hasErrors()) {
+			Map<String, String> errorMap = new HashMap<>();
+			
+			for(FieldError error : bindingResult.getFieldErrors()) {
+				errorMap.put(error.getField(), error.getDefaultMessage());
+			}
+			throw new CustomValidationException("유효성 검사 실패", errorMap);
+		} else {
+			// User <-- SignupDto
+			User user = signupDto.toEntity();
+			// log.info(user.toString());
+			User userEntity = authService.회원가입(user);
+			System.out.println(userEntity);
+			return "auth/signin"; // 회원가입이 진행되면 로그인창으로 이동
+		}
 	}
-	
 }
