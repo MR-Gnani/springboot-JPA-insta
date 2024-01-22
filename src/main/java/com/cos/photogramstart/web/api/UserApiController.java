a8bb8c0415ceb523cad88337b4d3ddd04c356620
MR NANI <flsk9698@naver.com>
2024-01-22 10:34:20 +0900

    5-4팔로잉/정보모달API

--- src/main/java/com/cos/photogramstart/web/api/UserApiController.java
+++ src/main/java/com/cos/photogramstart/web/api/UserApiController.java
@@ -1,22 +1,28 @@
 package com.cos.photogramstart.web.api;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.validation.Valid;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cos.photogramstart.Service.SubscribeService;
 import com.cos.photogramstart.Service.UserService;
 import com.cos.photogramstart.config.auth.PrincipalDetails;
 import com.cos.photogramstart.domain.user.User;
 import com.cos.photogramstart.handler.ex.CustomValidationApiException;
 import com.cos.photogramstart.web.dto.CMRespDto;
+import com.cos.photogramstart.web.dto.subscribe.SubscribeDto;
 import com.cos.photogramstart.web.dto.user.UserUpdateDto;
 
 import lombok.RequiredArgsConstructor;
@@ -26,6 +32,15 @@ import lombok.RequiredArgsConstructor;
 public class UserApiController {
 
 	private final UserService userService; // DI
+	private final SubscribeService subscribeService;
+	
+	@GetMapping("/api/user/{pageUserId}/subscribe")
+	public ResponseEntity<?> subscribeList(@PathVariable int pageUserId, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+		
+		List<SubscribeDto> subscribeDto = subscribeService.팔로잉리스트(principalDetails.getUser().getId(), pageUserId);
+		
+		return new ResponseEntity<>(new CMRespDto<>(1, "팔로잉 리스트 불러오기 성공", subscribeDto), HttpStatus.OK);
+	}
 	
 	@PutMapping("/api/user/{id}")
 		public CMRespDto<?> update(
